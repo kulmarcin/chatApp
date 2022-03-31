c407758a30f5c0ef7d67b8f62b1ea6eff7f7db98
Marcin <roozeek@icloud.com>
2022-03-31 12:57:08 +0200

    added server functionality with socket.io

---
+++ backend/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const socket = require('socket.io');
+const cors = require('cors');
+const { getCurrentUser, userDisconnect, joinUser } = require('./user');
+
+const app = express();
+app.use(express());
+
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+
+const server = app.listen(
+  port,
+  console.log(`Server is running on the port: ${port} `)
+);
+
+const io = socket(server);
+
+io.on('connection', socket => {
+
+  socket.on('joinRoom', ({ username, roomName }) => {
+    //create user
+    const user = joinUser(socket.id, username, roomName);
+    socket.join(user.room);
+
+    //Welcome message to user
+    socket.emit('message', {
+      userId: user.id,
+      username: user.username,
+      text: `Welcome ${user.username}`
+    });
+
+    //others see that user joined the chat
+    socket.broadcast.to(user.room).emit('message', {
+      userId: user.id,
+      username: user.username,
+      text: `${user.username} has joined the chat`
+    });
+  });
+
+  
+  socket.on('chat', text => {
+    //get user object
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', {
+      userId: user.id,
+      username: user.username,
+      text: text
+    });
+  });
+
+  
+  socket.on('disconnect', () => {
+    const user = userDisconnect(socket.id);
+
+    if (user) {
+        //message to all that user has disconnected
+      io.to(user.room).emit('message', {
+        userId: user.id,
+        username: user.username,
+        text: `${user.username} has left the room`
+      });
+    }
+  });
+});
